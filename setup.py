080382d7cf95f942804b31e8350e8458c83ae869
joaopna <joaopna@lccv.ufal.br>
2024-07-12 18:15:12 -0300

    adjustments for version 1.0.0 release

--- setup.py
+++ setup.py
@@ -7,15 +7,16 @@ setup(
     version='1.0.0',
     description='OpenID Connect client for Django applications',
     long_description='WIP',
-    author='Lucas S. Magalhães',
-    author_email='lucas.sampaio@intelie.com.br',
+    author='Lucas S. Magalhães, Daniel Pimentel, João Paulo de Araújo, Romero Malaquias',
+    author_email='lucas.sampaio@intelie.com.br, danielpimentel@lccv.ufal.br, joaopna@lccv.ufal.br, '
+                 'romero.malaquias@edge.ufal.br',
     packages=find_packages(exclude=['*.tests']),
     include_package_data=True,
     install_requires=[
-        'Django>=1.5',
+        'Django>=3.0,<4.2',
         'South==1.0.2',
         'pyjwkest==1.1.5',
-        'requests',
+        'requests==2.22.0',
     ],
     zip_safe=True
 )
